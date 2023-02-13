1c64d5f9bc8cc4c4fe1e2e6752e9104e8fc9648c
Keshav <keshavbhutaani1360@gmail.com>
2023-02-13 23:41:22 +0530

    modified Table.js and App.js by learning and adding states to dynamically change the webpage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,41 @@ import React,{ Component } from "react";
 import Table from "./Table";
 
 class App extends Component {
+    
+    state ={ 
+        characters : [
+            {
+                name : "Keshav",
+                state : "Haryana",
+            },
+            {
+                name : "Sachin",
+                state : "Maharashtra",
+            },
+            {
+                name : "Sahil",
+                state : "Assam",
+            },
+            {
+                name : "Vanshika",
+                state : "Uttar-Pradesh",
+            },
+        ]
+    }
+    removeCharacter =(index) => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((characters, i) => {
+                return i !== index;
+            }),
+        })
+    }
     render() {
+        const { characters } =this.state;
         return(
-            <div className="App">
-                <Table />
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
             </div>
         )
     }
--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -6,42 +6,52 @@ const TableHeader = () =>{
         <tr>
           <th>Name</th>
           <th>State</th>
+          <th>Remove</th>
         </tr>
       </thead>
     )
 }
 
-const TableBody =() =>{
-    return (
-        <tbody>
-        <tr>
-          <td>Keshav</td>
-          <td>Haryana</td>
-        </tr>
-        <tr>
-          <td>Sachin</td>
-          <td>Maharashtra</td>
-        </tr>
-        <tr>
-          <td>Sahil</td>
-          <td>Assam</td>
-        </tr>
-        <tr>
-          <td>Vanshika</td>
-          <td>Uttar-Pradesh</td>
-        </tr>
-      </tbody>
-    )
+const TableBody =(props) =>{
+    const rows =props.characterData.map((row, index) => {
+        return(
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.state}</td>
+                <td>
+                    <button onClick={ () => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
+    //     <tbody>
+    //     <tr>
+    //       <td>Keshav</td>
+    //       <td>Haryana</td>
+    //     </tr>
+    //     <tr>
+    //       <td>Sachin</td>
+    //       <td>Maharashtra</td>
+    //     </tr>
+    //     <tr>
+    //       <td>Sahil</td>
+    //       <td>Assam</td>
+    //     </tr>
+    //     <tr>
+    //       <td>Vanshika</td>
+    //       <td>Uttar-Pradesh</td>
+    //     </tr>
+    //   </tbody>
 }
-class Table extends Component {
-  render() {
+const Table = (props) =>  {
+    const { characterData, removeCharacter } = props;
     return (
       <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
       </table>
     )
-  }
 }
 
 export default Table
