82beae7d891f5eaa458f049d3556b244aea5fb82
Keshav <keshavbhutaani1360@gmail.com>
2023-02-13 20:56:29 +0530

    deleted all files except index.js ans index.css copied primitive css for our app and learned basic react rendering by some examples in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React , { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Keshav's React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
