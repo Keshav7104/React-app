38669b09e9e4668232670fbad4d8a9b649c31e63
Keshav <keshavbhutaani1360@gmail.com>
2023-02-13 22:40:03 +0530

    modified Table.js to break class components into simple components

--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react'
 
+const TableHeader = () =>{
+    return (
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>State</th>
+        </tr>
+      </thead>
+    )
+}
+
+const TableBody =() =>{
+    return (
+        <tbody>
+        <tr>
+          <td>Keshav</td>
+          <td>Haryana</td>
+        </tr>
+        <tr>
+          <td>Sachin</td>
+          <td>Maharashtra</td>
+        </tr>
+        <tr>
+          <td>Sahil</td>
+          <td>Assam</td>
+        </tr>
+        <tr>
+          <td>Vanshika</td>
+          <td>Uttar-Pradesh</td>
+        </tr>
+      </tbody>
+    )
+}
 class Table extends Component {
   render() {
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>State</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Keshav</td>
-            <td>Haryana</td>
-          </tr>
-          <tr>
-            <td>Sachin</td>
-            <td>Maharashtra</td>
-          </tr>
-          <tr>
-            <td>Sahil</td>
-            <td>Assam</td>
-          </tr>
-          <tr>
-            <td>Vanshika</td>
-            <td>Uttar-Pradesh</td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody />
       </table>
     )
   }
